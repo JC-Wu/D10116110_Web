06e7b3cca84e63f2a0c724409ee098bc6dd971e2
WWSJ <jacky19940328@gmail.com>
2014-12-21 16:14:35 +0800

    1.1.0
    start login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+<?php
+	$id=$_POST['id'];
+	$passwd=$_POST['passwd'];
+	$conn=mysql_connect('localhost','root','takming');
+	$sql="select * from user_profile where id='{$id}'";
+	$res=mysql_query($sql) or die("Can't find data");
+	$row=mysql_fetch_array($res);
+	loginSuccess=false;
+	if ($row && $row['passwd'])
+	{
+		if (md5($passwd)==$row['passwd'])
+		{
+			loginSuccess=true;
+		}
+	}
+	if(loginSuccess)
+	{
+		echo "登入成功！";
+	}
+	else
+	{
+		echo "登入失敗！請檢查帳號密碼";
+	}
+?>
+</html>
