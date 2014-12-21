02185f4a8951e2a42b304946d7691916b036f917
WWSJ <jacky19940328@gmail.com>
2014-12-21 16:18:17 +0800

    1.1.1
    update $ & require

--- login.php
+++ login.php
@@ -1,12 +1,13 @@
 <html>
 <?php
+require "c:/etc/config.php";
 	$id=$_POST['id'];
 	$passwd=$_POST['passwd'];
 	$conn=mysql_connect('localhost','root','takming');
 	$sql="select * from user_profile where id='{$id}'";
 	$res=mysql_query($sql) or die("Can't find data");
 	$row=mysql_fetch_array($res);
-	loginSuccess=false;
+	$loginSuccess=false;
 	if ($row && $row['passwd'])
 	{
 		if (md5($passwd)==$row['passwd'])
